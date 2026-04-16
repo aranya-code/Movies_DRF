be4058f88fc038941371a67998e3ffd2172b6da8
Aranya Majumdar <74165561+aranya-code@users.noreply.github.com>
2026-04-16 22:35:32 +0530

    add views

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,18 +1,35 @@
 from rest_framework import serializers
 from movies.models import Movies, Rating
 
-
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model= Movies
-        fields= '__all__'
-
-    def movie_validator(self, name):
-        if Movies.objects.filter(movie_name = name).exists():
-            raise serializers.ValidationError(detail='Movie name already exists.')
-        return name
-
 class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model= Rating
         fields= '__all__'
+
+    def validate(self, data):
+        if Rating.objects.filter(
+            reviewer=data['reviewer'],
+            movie=data['movie']
+        ).exists():
+            raise serializers.ValidationError(
+                "You have already posted a review on this movie."
+            )
+        return data
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    ratings = serializers.SerializerMethodField()
+    class Meta:
+        model= Movies
+        fields= '__all__'
+
+    def validate_movie_name(self, value):
+        if Movies.objects.filter(movie_name = value).exists():
+            raise serializers.ValidationError(detail='Movie name already exists.')
+        return value
+
+
+    def get_ratings(self, obj):
+        ratings = obj.ratings.all().order_by('rating')
+        return RatingSerializer(ratings, many=True).data
+    
--- movies/views.py
+++ movies/views.py
@@ -20,12 +20,63 @@ class MovieView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def get(self, request):
-        movie_names = Movies.objects.all
+        movie_names = Movies.objects.all()
         if movie_names.exists():
             serializer = MovieSerializer(movie_names, many= True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_400_BAD_REQUEST)
     
+class RatingView(APIView):
+
+    def post(self, request):
+        serializer= RatingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Review posted successfully',
+                             'Data': serializer.data
+                             },
+                             status=status.HTTP_201_CREATED)
+        
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+    
+class MovieDetailView(APIView):
+
+    def get_movie_by_id(self, id):
+        try:
+            movie = Movies.objects.get(id= id)
+        except Movies.DoesNotExist:
+            return None
+
+    
+    def get(self, request, id):
+        try:
+            movie = self.get_movie_by_id(id)
+            serializer = MovieSerializer(movie)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
+            return Response({'message': 'No movie exists with this id'},
+                            status= status.HTTP_400_BAD_REQUEST)
+        
+    def put(self, request, id):
+        try:
+            movie = self.get_movie_by_id(id)
+            serializer = MovieSerializer(movie, data={"budget": request.data.get("budget")}, partial= True)
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_200_OK)
+        except:
+            return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, id):
+        try:
+            movie = self.get_movie_by_id(id)
+            movie.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response({'message': 'No movie exists with this id'},
+                            status= status.HTTP_400_BAD_REQUEST)
 
 
         
